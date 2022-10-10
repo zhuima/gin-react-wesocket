ca2ba179609143a86c96574f2ea4c7c32e73d2e9
zhuima <zhuima314@gmail.com>
2022-10-10 16:57:22 +0800

    添加水印

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
+import WaterMark from "watermark-component-for-react";
 import Header from "./components/Header";
 import Input from "./components/Input";
 
 // import "./App.css";
 
 function App() {
+  const content = `Demo演示，请勿喷 `;
   return (
     <div className="App">
-      <Header />
+      <WaterMark content={content}>
+        <Header />
 
-      <Input />
+        <Input />
+      </WaterMark>
     </div>
   );
 }
